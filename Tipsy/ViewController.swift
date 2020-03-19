ed444e855528958b5a51feae889124a4381de486
zexi yu <eezxyu@gmail.com>
2020-03-19 12:54:23 +0800

    Connect IBOutlets and IBActions for the Calculator page. 1. make the keyboard disappear when tapping the screen 2. make the tapped tip button selected 3. make the stepper work Next Step: implement the calculation logic and route to the result page

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var zeroPCTButton: UIButton!
+    @IBOutlet weak var tenPCTButton: UIButton!
+    @IBOutlet weak var twentyPCTButton: UIButton!
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var splitLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
-
-
+    
+    @IBAction func tipSelected(_ sender: UIButton) {
+        zeroPCTButton.isSelected = false
+        tenPCTButton.isSelected = false
+        twentyPCTButton.isSelected = false
+        
+        if sender.currentTitle == "0%" {
+            zeroPCTButton.isSelected = true
+        } else if sender.currentTitle == "10%" {
+            tenPCTButton.isSelected = true
+        } else {
+            twentyPCTButton.isSelected = true
+        }
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        let splitNumber = Int(sender.value).description
+        splitLabel.text = splitNumber
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let billAmount = billTextField.text
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
 }
 
