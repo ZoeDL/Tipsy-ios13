4eb1b9ac997fd9e46f47a29cc042f1404afa19fc
zexi yu <eezxyu@gmail.com>
2020-03-19 13:53:51 +0800

    Add a ResultViewController and make the segue work Pass all necessary data to the result page Next Step: refactor the code to implement MVC pattern

---
+++ Tipsy/View/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by 余泽熙 on 3/19/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var splitAmount: Double?
+    var splitNumber: Int?
+    var tipAmount: Double?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(splitAmount!)
+        print(splitNumber!)
+        print(tipAmount!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tipsy/View/ViewController.swift
+++ Tipsy/View/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var zeroPCTButton: UIButton!
     @IBOutlet weak var tenPCTButton: UIButton!
     @IBOutlet weak var twentyPCTButton: UIButton!
+    @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var splitLabel: UILabel!
-    
+    var tip = 0.1
+    var splitAmount: Double = 0
+    var splitNumber: Int = 2
     override func viewDidLoad() {
         super.viewDidLoad()
         //Looks for single or multiple taps.
@@ -30,10 +33,13 @@ class ViewController: UIViewController {
         
         if sender.currentTitle == "0%" {
             zeroPCTButton.isSelected = true
+            tip = 0
         } else if sender.currentTitle == "10%" {
             tenPCTButton.isSelected = true
+            tip = 0.1
         } else {
             twentyPCTButton.isSelected = true
+            tip = 0.2
         }
     }
     
@@ -44,11 +50,34 @@ class ViewController: UIViewController {
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         let billAmount = billTextField.text
+        if let billAmountUnwrapped = billAmount {
+            if let billAmountDouble = Double(billAmountUnwrapped) {
+                splitNumber = Int(stepper.value)
+                let billAmountWithTips = billAmountDouble * (1 + tip)
+                splitAmount = billAmountWithTips / Double(splitNumber)
+                
+                print("average : \(splitAmount)")
+                
+                self.performSegue(withIdentifier: "goToResultPage", sender: self)
+            } else {
+                print("invalid bill total")
+            }
+        }
     }
     
     @objc func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResultPage" {
+            let distinationVC = segue.destination as! ResultViewController
+            distinationVC.splitAmount = splitAmount
+            distinationVC.splitNumber = splitNumber
+            distinationVC.tipAmount = tip
+          
+        }
     }
     
 }
